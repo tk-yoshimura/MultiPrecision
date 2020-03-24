940ebf63f782bcb53eb970d635597c781af38c0c
tk-yoshimura <52984388+tk-yoshimura@users.noreply.github.com>
2020-03-24 16:09:20 +0900

    Create MultiPrecisionTest_minmax.cs

---
+++ MultiPrecisionTest/MultiPrecision/MathFunctions/MultiPrecisionTest_minmax.cs
@@ -0,0 +1,41 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MultiPrecision;
+using System;
+using System.Linq;
+
+namespace MultiPrecisionTest.Functions {
+    public partial class MultiPrecisionTest {
+        [TestMethod]
+        public void MinMaxTest() {
+            MultiPrecision<Pow2.N8>[] vs = {
+                MultiPrecision<Pow2.N8>.NegativeInfinity,
+                MultiPrecision<Pow2.N8>.MinValue,
+                "-1e100",
+                "-1e-100",
+                -MultiPrecision<Pow2.N8>.Epsilon,
+                MultiPrecision<Pow2.N8>.MinusZero,
+                MultiPrecision<Pow2.N8>.Zero,
+                MultiPrecision<Pow2.N8>.Epsilon,
+                "1e-100",
+                "1e100",
+                MultiPrecision<Pow2.N8>.MaxValue,
+                MultiPrecision<Pow2.N8>.PositiveInfinity
+            };
+
+            foreach (var u in vs) {
+                foreach (var v in vs) {
+                    var max = MultiPrecision<Pow2.N8>.Max(u, v);
+                    var min = MultiPrecision<Pow2.N8>.Min(u, v);
+
+                    Console.WriteLine(u);
+                    Console.WriteLine(v);
+
+                    Console.WriteLine($"max : {max}");
+                    Console.WriteLine($"min : {min}");
+
+                    Console.Write("\n");
+                }
+            }
+        }
+    }
+}
