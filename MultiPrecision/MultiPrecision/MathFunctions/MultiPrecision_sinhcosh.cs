f8af1b0e1d81578c6b8a16cfad3face72a910006
tk-yoshimura <52984388+tk-yoshimura@users.noreply.github.com>
2020-03-24 15:15:21 +0900

    + sinh cosh tanh

---
+++ MultiPrecision/MultiPrecision/MathFunctions/MultiPrecision_sinhcosh.cs
@@ -0,0 +1,19 @@
+﻿namespace MultiPrecision {
+
+    public sealed partial class MultiPrecision<N> {
+
+        public static MultiPrecision<N> Sinh(MultiPrecision<N> x) {
+            return Ldexp(Exp(x) - Exp(-x), -1);
+        }
+
+        public static MultiPrecision<N> Cosh(MultiPrecision<N> x) {
+            return Ldexp(Exp(x) + Exp(-x), -1);
+        }
+
+        public static MultiPrecision<N> Tanh(MultiPrecision<N> x) {
+            MultiPrecision<N> exp_2x = Exp(Ldexp(x, 1));
+
+            return (exp_2x + MinusOne) / (exp_2x + One);
+        }
+    }
+}
---
+++ MultiPrecisionTest/MultiPrecision/MathFunctions/MultiPrecisionTest_sinhcosh.cs
@@ -0,0 +1,116 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MultiPrecision;
+using System;
+
+namespace MultiPrecisionTest.Functions {
+    public partial class MultiPrecisionTest {
+
+        [TestMethod]
+        public void TanhTest() {
+            for (Int64 i = -1000; i <= 1000; i++) {
+                MultiPrecision<Pow2.N8> x = (MultiPrecision<Pow2.N8>)i / 250;
+                MultiPrecision<Pow2.N8> y_actual = MultiPrecision<Pow2.N8>.Tanh(x);
+                double y_expect = Math.Tanh((double)x);
+
+                Console.WriteLine((double)x);
+                Console.WriteLine((double)y_actual);
+                Console.WriteLine((double)y_expect);
+                Assert.AreEqual(y_expect, (double)y_actual, Math.Abs(y_expect * 1e-5) + 1e-10);
+            }
+
+            for (Int64 i = -1000; i <= 1000; i++) {
+                MultiPrecision<Pow2.N8> x = 65536 + (MultiPrecision<Pow2.N8>)i / 250;
+                MultiPrecision<Pow2.N8> y_actual = MultiPrecision<Pow2.N8>.Tanh(x);
+                double y_expect = Math.Tanh((double)x);
+
+                Console.WriteLine((double)x);
+                Console.WriteLine((double)y_actual);
+                Console.WriteLine((double)y_expect);
+                Assert.AreEqual(y_expect, (double)y_actual, Math.Abs(y_expect * 1e-5) + 1e-10);
+            }
+
+            for (Int64 i = -1000; i <= 1000; i++) {
+                MultiPrecision<Pow2.N8> x = -65536 + (MultiPrecision<Pow2.N8>)i / 250;
+                MultiPrecision<Pow2.N8> y_actual = MultiPrecision<Pow2.N8>.Tanh(x);
+                double y_expect = Math.Tanh((double)x);
+
+                Console.WriteLine((double)x);
+                Console.WriteLine((double)y_actual);
+                Console.WriteLine((double)y_expect);
+                Assert.AreEqual(y_expect, (double)y_actual, Math.Abs(y_expect * 1e-5) + 1e-10);
+            }
+        }
+
+        [TestMethod]
+        public void SinhTest() {
+            for (Int64 i = -1000; i <= 1000; i++) {
+                MultiPrecision<Pow2.N8> x = (MultiPrecision<Pow2.N8>)i / 250;
+                MultiPrecision<Pow2.N8> y_actual = MultiPrecision<Pow2.N8>.Sinh(x);
+                double y_expect = Math.Sinh((double)x);
+
+                Console.WriteLine((double)x);
+                Console.WriteLine((double)y_actual);
+                Console.WriteLine((double)y_expect);
+                Assert.AreEqual(y_expect, (double)y_actual, Math.Abs(y_expect * 1e-5) + 1e-10);
+            }
+
+            for (Int64 i = -1000; i <= 1000; i++) {
+                MultiPrecision<Pow2.N8> x = 65536 + (MultiPrecision<Pow2.N8>)i / 250;
+                MultiPrecision<Pow2.N8> y_actual = MultiPrecision<Pow2.N8>.Sinh(x);
+                double y_expect = Math.Sinh((double)x);
+
+                Console.WriteLine((double)x);
+                Console.WriteLine((double)y_actual);
+                Console.WriteLine((double)y_expect);
+                Assert.AreEqual(y_expect, (double)y_actual, Math.Abs(y_expect * 1e-5) + 1e-10);
+            }
+
+            for (Int64 i = -1000; i <= 1000; i++) {
+                MultiPrecision<Pow2.N8> x = -65536 + (MultiPrecision<Pow2.N8>)i / 250;
+                MultiPrecision<Pow2.N8> y_actual = MultiPrecision<Pow2.N8>.Sinh(x);
+                double y_expect = Math.Sinh((double)x);
+
+                Console.WriteLine((double)x);
+                Console.WriteLine((double)y_actual);
+                Console.WriteLine((double)y_expect);
+                Assert.AreEqual(y_expect, (double)y_actual, Math.Abs(y_expect * 1e-5) + 1e-10);
+            }
+        }
+
+        [TestMethod]
+        public void CoshTest() {
+            for (Int64 i = -1000; i <= 1000; i++) {
+                MultiPrecision<Pow2.N8> x = (MultiPrecision<Pow2.N8>)i / 250;
+                MultiPrecision<Pow2.N8> y_actual = MultiPrecision<Pow2.N8>.Cosh(x);
+                double y_expect = Math.Cosh((double)x);
+
+                Console.WriteLine((double)x);
+                Console.WriteLine((double)y_actual);
+                Console.WriteLine((double)y_expect);
+                Assert.AreEqual(y_expect, (double)y_actual, Math.Abs(y_expect * 1e-5) + 1e-10);
+            }
+
+            for (Int64 i = -1000; i <= 1000; i++) {
+                MultiPrecision<Pow2.N8> x = 65536 + (MultiPrecision<Pow2.N8>)i / 250;
+                MultiPrecision<Pow2.N8> y_actual = MultiPrecision<Pow2.N8>.Cosh(x);
+                double y_expect = Math.Cosh((double)x);
+
+                Console.WriteLine((double)x);
+                Console.WriteLine((double)y_actual);
+                Console.WriteLine((double)y_expect);
+                Assert.AreEqual(y_expect, (double)y_actual, Math.Abs(y_expect * 1e-5) + 1e-10);
+            }
+
+            for (Int64 i = -1000; i <= 1000; i++) {
+                MultiPrecision<Pow2.N8> x = -65536 + (MultiPrecision<Pow2.N8>)i / 250;
+                MultiPrecision<Pow2.N8> y_actual = MultiPrecision<Pow2.N8>.Cosh(x);
+                double y_expect = Math.Cosh((double)x);
+
+                Console.WriteLine((double)x);
+                Console.WriteLine((double)y_actual);
+                Console.WriteLine((double)y_expect);
+                Assert.AreEqual(y_expect, (double)y_actual, Math.Abs(y_expect * 1e-5) + 1e-10);
+            }
+        }
+    }
+}
