a7dd0cc5dbb23e490df02291488a626f173861ec
tk-yoshimura <yoshimura8192@gmail.com>
2020-06-19 17:31:00 +0900

    fix gamma.test

--- MultiPrecisionTest/MultiPrecision/MathFunctions/MultiPrecisionTest_gamma.cs
+++ MultiPrecisionTest/MultiPrecision/MathFunctions/MultiPrecisionTest_gamma.cs
@@ -6,6 +6,54 @@ using System;
 
 namespace MultiPrecisionTest.Functions {
     public partial class MultiPrecisionTest {
+
+        public static double GammaApprox(double z) {
+            double[] p = {
+                676.5203681218851,
+                -1259.1392167224028,
+                771.32342877765313,
+                -176.61502916214059,
+                12.507343278686905,
+                -0.13857109526572012,
+                9.9843695780195716e-6,
+                1.5056327351493116e-7
+            };
+
+            if (z < 0.5) {
+                return Math.PI / (Math.Sin(Math.PI * z) * GammaApprox(1 - z));
+            }
+            else {
+                z -= 1;
+                double x = 1 - double.Epsilon;
+                for (int i = 0; i < p.Length; i++) {
+                    x += p[i] / (z + i + 1);
+                }
+                double t = z + p.Length - 0.5;
+
+                return Math.Sqrt(2 * Math.PI) * Math.Pow(t, (z + 0.5)) * Math.Exp(-t) * x;
+            }
+        }
+
+        public static double LogGammaApprox(double z) {
+            return z >= 0 ? Math.Log(GammaApprox(z)) : double.NaN;
+        }
+
+        [TestMethod]
+        public void GammaApproxTest() {
+            Assert.AreEqual(1, GammaApprox(1), 1e-5);
+            Assert.AreEqual(1, GammaApprox(2), 1e-5);
+            Assert.AreEqual(2, GammaApprox(3), 1e-5);
+            Assert.AreEqual(6, GammaApprox(4), 1e-5);
+            Assert.AreEqual(24, GammaApprox(5), 1e-5);
+
+            Assert.AreEqual(Math.Sqrt(Math.PI) * 4 / 3, GammaApprox(-1.5), 1e-5);
+            Assert.AreEqual(Math.Sqrt(Math.PI) * -2, GammaApprox(-0.5), 1e-5);
+            Assert.AreEqual(Math.Sqrt(Math.PI), GammaApprox(0.5), 1e-5);
+            Assert.AreEqual(Math.Sqrt(Math.PI) / 2, GammaApprox(1.5), 1e-5);
+            Assert.AreEqual(Math.Sqrt(Math.PI) * 3 / 4, GammaApprox(2.5), 1e-5);
+            Assert.AreEqual(Math.Sqrt(Math.PI) * 15 / 8, GammaApprox(3.5), 1e-5);
+        }
+
         [TestMethod]
         public void LogGammaTest() {
             for (int i = 0; i < 200; i++) {
@@ -16,6 +64,10 @@ namespace MultiPrecisionTest.Functions {
                 Console.WriteLine(y);
                 Console.WriteLine($"{y.Sign} {y.Exponent}, {UIntUtil.ToHexcode(y.Mantissa)}");
                 Console.Write("\n");
+
+                if (y.IsFinite) {
+                    Assert.AreEqual(LogGammaApprox((double)x), (double)y, Math.Abs(LogGammaApprox((double)x) * 1e-5) + 1e-12, x.ToString());
+                }
             }
         }
 
@@ -29,6 +81,10 @@ namespace MultiPrecisionTest.Functions {
                 Console.WriteLine(y);
                 Console.WriteLine($"{y.Sign} {y.Exponent}, {UIntUtil.ToHexcode(y.Mantissa)}");
                 Console.Write("\n");
+
+                if (y.IsFinite) {
+                    Assert.AreEqual(GammaApprox((double)x), (double)y, Math.Abs(GammaApprox((double)x)) * 1e-5 + 1e-12, x.ToString());
+                }
             }
         }
 
