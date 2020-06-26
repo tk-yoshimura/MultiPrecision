74d9240724f5cad2aed04936ad782b69f0cd1624
tk-yoshimura <yoshimura8192@gmail.com>
2020-06-26 12:43:11 +0900

    + eulergamma

---
+++ MultiPrecisionTest/MultiPrecision/MathConstants/MultiPrecisionTest_eulergamma.cs
@@ -0,0 +1,42 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using MultiPrecision;
+
+using System;
+
+namespace MultiPrecisionTest.Constants {
+    public partial class MultiPrecisionTest {
+        [TestMethod]
+        public void EulerGammaTest() {
+            MultiPrecision<Pow2.N8> eulergamma = MultiPrecision<Pow2.N8>.EulerGamma;
+
+            Console.WriteLine(eulergamma);
+            Console.WriteLine(eulergamma.ToHexcode());
+
+            TestTool.Tolerance(0.57721566490153286, eulergamma);
+        }
+
+        [TestMethod]
+        public void EulerGammaDigitsTest() {
+            Console.WriteLine(MultiPrecision<Pow2.N8>.EulerGamma);
+            Console.WriteLine(MultiPrecision<Pow2.N8>.EulerGamma.ToHexcode());
+
+            Console.WriteLine(MultiPrecision<Pow2.N16>.EulerGamma);
+            Console.WriteLine(MultiPrecision<Pow2.N16>.EulerGamma.ToHexcode());
+
+            Console.WriteLine(MultiPrecision<Pow2.N32>.EulerGamma);
+            Console.WriteLine(MultiPrecision<Pow2.N32>.EulerGamma.ToHexcode());
+
+            Console.WriteLine(MultiPrecision<Pow2.N64>.EulerGamma);
+            Console.WriteLine(MultiPrecision<Pow2.N64>.EulerGamma.ToHexcode());
+
+            Console.WriteLine(MultiPrecision<Pow2.N128>.EulerGamma.ToHexcode());
+
+            Console.WriteLine(MultiPrecision<Pow2.N256>.EulerGamma.ToHexcode());
+
+            Console.WriteLine(MultiPrecision<Pow2.N512>.EulerGamma.ToHexcode());
+
+            Console.WriteLine(MultiPrecision<Pow2.N1024>.EulerGamma.ToHexcode());
+        }
+    }
+}
