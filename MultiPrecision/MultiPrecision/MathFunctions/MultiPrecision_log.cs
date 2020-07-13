9b82c0b7e74ccb724190981c7e3d94fb77ed9923
tk-yoshimura <52984388+tk-yoshimura@users.noreply.github.com>
2020-07-13 16:34:28 +0900

    + log1p

--- MultiPrecision/MultiPrecision/MathFunctions/MultiPrecision_log.cs
+++ MultiPrecision/MultiPrecision/MathFunctions/MultiPrecision_log.cs
@@ -15,5 +15,31 @@
 
             return y;
         }
+
+        public static MultiPrecision<N> Log1p(MultiPrecision<N> x) {
+            const int exp_threshold = 2;
+
+            if (x.Exponent >= -exp_threshold) {
+                return Log(One + x);
+            }
+
+            MultiPrecision<Plus1<N>> x_expand = MultiPrecisionUtil.Convert<Plus1<N>, N>(x);
+            MultiPrecision<Plus1<N>> w = x_expand * x_expand;
+            MultiPrecision<Plus1<N>> y = x_expand;
+            MultiPrecision<Plus1<N>> z = w;
+
+            for(long i = 2, f = 2; i < Bits; i += exp_threshold * 2, f += 2){
+                MultiPrecision<Plus1<N>> dy = z * (x_expand * f - MultiPrecision<Plus1<N>>.One * (f + 1)) / checked(f * (f + 1));
+                y += dy;
+
+                if (dy.IsZero || y.Exponent - dy.Exponent > Bits) {
+                    break;
+                }
+
+                z *= w;
+            }
+
+            return MultiPrecisionUtil.Convert<N, Plus1<N>>(y);
+        }
     }
 }
--- MultiPrecisionTest/MultiPrecision/MathFunctions/MultiPrecisionTest_log.cs
+++ MultiPrecisionTest/MultiPrecision/MathFunctions/MultiPrecisionTest_log.cs
@@ -18,5 +18,44 @@ namespace MultiPrecisionTest.Functions {
                 TestTool.Tolerance(Math.Log((double)x), y);
             }
         }
+
+        [TestMethod]
+        public void Log1pTest() {
+            foreach(MultiPrecision<Pow2.N8> x in TestTool.PositiveRangeSet<Pow2.N8>()) { 
+
+                MultiPrecision<Pow2.N8> y = MultiPrecision<Pow2.N8>.Log1p(x - 1);
+
+                Console.WriteLine(x - 1);
+                Console.WriteLine(y);
+                
+                TestTool.Tolerance(Math.Log((double)x), y);
+            }
+        }
+
+        [TestMethod]
+        public void Log1pBorderTest() {
+            MultiPrecision<Pow2.N8>[] borders = new MultiPrecision<Pow2.N8>[] { 0, -0.25, 0.25 };
+
+            foreach (MultiPrecision<Pow2.N8> b in borders) {
+                foreach (MultiPrecision<Pow2.N8> x in TestTool.EnumerateNeighbor(b, n: 2)) {
+
+                    MultiPrecision<Pow2.N8> y = MultiPrecision<Pow2.N8>.Log1p(x);
+
+                    Console.WriteLine(x);
+                    Console.WriteLine(x.ToHexcode());
+                    Console.WriteLine(y);
+                    Console.WriteLine(y.ToHexcode());
+                    Console.Write("\n");
+
+                    if ((double)x == 0) {
+                        continue;
+                    }
+
+                    TestTool.Tolerance(Math.Log(1 + (double)x), y, ignore_sign: true);
+                }
+
+                Console.Write("\n");
+            }
+        }
     }
 }
