3a1ac6d324160b6f60df6dd5f4d50eadf985305b
tk-yoshimura <52984388+tk-yoshimura@users.noreply.github.com>
2020-08-05 16:43:01 +0900

    + fraction

---
+++ MultiPrecision/Fraction/Fraction.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Numerics;
+
+namespace MultiPrecision {
+    internal class Fraction {
+        public BigInteger Numer { private set; get; }
+        public BigInteger Denom { private set; get; }
+
+        public Fraction(BigInteger n) { 
+            this.Numer = n;
+            this.Denom = 1;
+        }
+
+        public Fraction(BigInteger numer, BigInteger denom) { 
+            this.Numer = numer;
+            this.Denom = denom;
+
+            Reduce();
+        }
+
+        public static Fraction operator+(Fraction v1, Fraction v2) { 
+            return new Fraction(v1.Numer * v2.Denom + v2.Numer * v1.Denom, v1.Denom * v2.Denom);
+        }
+
+        public static Fraction operator-(Fraction v1, Fraction v2) { 
+            return new Fraction(v1.Numer * v2.Denom - v2.Numer * v1.Denom, v1.Denom * v2.Denom);
+        }
+
+        public static Fraction operator*(Fraction v1, Fraction v2) { 
+            return new Fraction(v1.Numer * v2.Numer, v1.Denom * v2.Denom);
+        }
+
+        public static Fraction operator*(Fraction v, BigInteger n) { 
+            return new Fraction(v.Numer * n, v.Denom);
+        }
+
+        public static Fraction operator/(Fraction v1, Fraction v2) { 
+            return new Fraction(v1.Numer * v2.Denom, v1.Denom * v2.Numer);
+        }
+
+        public static Fraction operator/(Fraction v, BigInteger n) { 
+            return new Fraction(v.Numer, v.Denom / n);
+        }
+
+        public void Reduce() { 
+            if (Denom == 0){
+                throw new DivideByZeroException();
+            }
+
+            if (Numer == 0){
+                Denom = 1;
+                return;
+            }
+
+            int sign = ((Numer < 0) ^ (Denom < 0)) ? -1 : +1;
+            BigInteger n = Numer, d = Denom;
+
+            d = BigInteger.Abs(d);
+            n = BigInteger.Abs(n);
+
+            if (n > d){
+                BigInteger temp = n;
+                n = d;
+                d = temp;
+            }
+
+            BigInteger rem;
+            while((rem = d % n) != 0){
+                d = n;
+                n = rem;
+            }
+
+            Numer = (sign > 0 ? BigInteger.Abs(Numer) : -BigInteger.Abs(Numer)) / n;
+            Denom = BigInteger.Abs(Denom) / n;
+        }
+
+        public override string ToString() {
+            return $"{Numer}/{Denom}";
+        }
+
+        public override int GetHashCode() {
+            return Numer.GetHashCode() ^ Denom.GetHashCode();
+        }
+
+        public override bool Equals(object obj) {
+            return obj is Fraction v && Numer == v.Numer && Denom == v.Denom;
+        }
+
+        public MultiPrecision<N> ToMultiPrecision<N>() where N : struct, IConstant { 
+            MultiPrecision<N> n = $"{Numer}", d = $"{Denom}";
+
+            return n / d;
+        }
+    }
+}
---
+++ MultiPrecisionTest/Fraction/Fraction.cs
@@ -0,0 +1,203 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using MultiPrecision;
+
+namespace MultiPrecisionTest.Fractions {
+    [TestClass]
+    public partial class FractionTest {
+        [TestMethod]
+        public void CreateTest() {
+            Fraction v1 = new Fraction(6, 12);
+
+            Assert.AreEqual(1, v1.Numer);
+            Assert.AreEqual(2, v1.Denom);
+
+            Fraction v2 = new Fraction(-6, 12);
+
+            Assert.AreEqual(-1, v2.Numer);
+            Assert.AreEqual(2, v2.Denom);
+
+            Fraction v3 = new Fraction(6, 12);
+
+            Assert.AreEqual(1, v3.Numer);
+            Assert.AreEqual(2, v3.Denom);
+
+            Fraction v4 = new Fraction(6, -12);
+
+            Assert.AreEqual(-1, v4.Numer);
+            Assert.AreEqual(2, v4.Denom);
+        }
+
+        [TestMethod]
+        public void AddTest() {
+            Fraction v1 = new Fraction(1, 2) + new Fraction(1, 3);
+
+            Assert.AreEqual(5, v1.Numer);
+            Assert.AreEqual(6, v1.Denom);
+
+            Fraction v2 = new Fraction(1, 2) + new Fraction(1, 2);
+
+            Assert.AreEqual(1, v2.Numer);
+            Assert.AreEqual(1, v2.Denom);
+
+            Fraction v3 = new Fraction(2, 1) + new Fraction(1, 2);
+
+            Assert.AreEqual(5, v3.Numer);
+            Assert.AreEqual(2, v3.Denom);
+
+            Fraction v4 = new Fraction(1, 6) + new Fraction(1, 4);
+
+            Assert.AreEqual(5, v4.Numer);
+            Assert.AreEqual(12, v4.Denom);
+
+            Fraction v5 = new Fraction(1, 2) + new Fraction(-1, 3);
+
+            Assert.AreEqual(1, v5.Numer);
+            Assert.AreEqual(6, v5.Denom);
+
+            Fraction v6 = new Fraction(1, 2) + new Fraction(-1, 2);
+
+            Assert.AreEqual(0, v6.Numer);
+            Assert.AreEqual(1, v6.Denom);
+
+            Fraction v7 = new Fraction(2, 1) + new Fraction(-1, 2);
+
+            Assert.AreEqual(3, v7.Numer);
+            Assert.AreEqual(2, v7.Denom);
+
+            Fraction v8 = new Fraction(1, 6) + new Fraction(-1, 4);
+
+            Assert.AreEqual(-1, v8.Numer);
+            Assert.AreEqual(12, v8.Denom);
+        }
+
+        [TestMethod]
+        public void SubTest() {
+            Fraction v1 = new Fraction(1, 2) - new Fraction(1, 3);
+
+            Assert.AreEqual(1, v1.Numer);
+            Assert.AreEqual(6, v1.Denom);
+
+            Fraction v2 = new Fraction(1, 2) - new Fraction(1, 2);
+
+            Assert.AreEqual(0, v2.Numer);
+            Assert.AreEqual(1, v2.Denom);
+
+            Fraction v3 = new Fraction(2, 1) - new Fraction(1, 2);
+
+            Assert.AreEqual(3, v3.Numer);
+            Assert.AreEqual(2, v3.Denom);
+
+            Fraction v4 = new Fraction(1, 6) - new Fraction(1, 4);
+
+            Assert.AreEqual(-1, v4.Numer);
+            Assert.AreEqual(12, v4.Denom);
+
+            Fraction v5 = new Fraction(1, 2) - new Fraction(-1, 3);
+
+            Assert.AreEqual(5, v5.Numer);
+            Assert.AreEqual(6, v5.Denom);
+
+            Fraction v6 = new Fraction(1, 2) - new Fraction(-1, 2);
+
+            Assert.AreEqual(1, v6.Numer);
+            Assert.AreEqual(1, v6.Denom);
+
+            Fraction v7 = new Fraction(2, 1) - new Fraction(-1, 2);
+
+            Assert.AreEqual(5, v7.Numer);
+            Assert.AreEqual(2, v7.Denom);
+
+            Fraction v8 = new Fraction(1, 6) - new Fraction(-1, 4);
+
+            Assert.AreEqual(5, v8.Numer);
+            Assert.AreEqual(12, v8.Denom);
+        }
+
+        [TestMethod]
+        public void MulTest() {
+            Fraction v1 = new Fraction(1, 2) * new Fraction(1, 3);
+
+            Assert.AreEqual(1, v1.Numer);
+            Assert.AreEqual(6, v1.Denom);
+
+            Fraction v2 = new Fraction(1, 2) * new Fraction(1, 2);
+
+            Assert.AreEqual(1, v2.Numer);
+            Assert.AreEqual(4, v2.Denom);
+
+            Fraction v3 = new Fraction(2, 1) * new Fraction(1, 2);
+
+            Assert.AreEqual(1, v3.Numer);
+            Assert.AreEqual(1, v3.Denom);
+
+            Fraction v4 = new Fraction(1, 6) * new Fraction(1, 4);
+
+            Assert.AreEqual(1, v4.Numer);
+            Assert.AreEqual(24, v4.Denom);
+
+            Fraction v5 = new Fraction(1, 2) * new Fraction(-1, 3);
+
+            Assert.AreEqual(-1, v5.Numer);
+            Assert.AreEqual(6, v5.Denom);
+
+            Fraction v6 = new Fraction(1, 2) * new Fraction(-1, 2);
+
+            Assert.AreEqual(-1, v6.Numer);
+            Assert.AreEqual(4, v6.Denom);
+
+            Fraction v7 = new Fraction(2, 1) * new Fraction(-1, 2);
+
+            Assert.AreEqual(-1, v7.Numer);
+            Assert.AreEqual(1, v7.Denom);
+
+            Fraction v8 = new Fraction(1, 6) * new Fraction(-1, 4);
+
+            Assert.AreEqual(-1, v8.Numer);
+            Assert.AreEqual(24, v8.Denom);
+        }
+
+        [TestMethod]
+        public void DivTest() {
+            Fraction v1 = new Fraction(1, 2) / new Fraction(1, 3);
+
+            Assert.AreEqual(3, v1.Numer);
+            Assert.AreEqual(2, v1.Denom);
+
+            Fraction v2 = new Fraction(1, 2) / new Fraction(1, 2);
+
+            Assert.AreEqual(1, v2.Numer);
+            Assert.AreEqual(1, v2.Denom);
+
+            Fraction v3 = new Fraction(2, 1) / new Fraction(1, 2);
+
+            Assert.AreEqual(4, v3.Numer);
+            Assert.AreEqual(1, v3.Denom);
+
+            Fraction v4 = new Fraction(1, 6) / new Fraction(1, 4);
+
+            Assert.AreEqual(2, v4.Numer);
+            Assert.AreEqual(3, v4.Denom);
+
+            Fraction v5 = new Fraction(1, 2) / new Fraction(-1, 3);
+
+            Assert.AreEqual(-3, v5.Numer);
+            Assert.AreEqual(2, v5.Denom);
+
+            Fraction v6 = new Fraction(1, 2) / new Fraction(-1, 2);
+
+            Assert.AreEqual(-1, v6.Numer);
+            Assert.AreEqual(1, v6.Denom);
+
+            Fraction v7 = new Fraction(2, 1) / new Fraction(-1, 2);
+
+            Assert.AreEqual(-4, v7.Numer);
+            Assert.AreEqual(1, v7.Denom);
+
+            Fraction v8 = new Fraction(1, 6) / new Fraction(-1, 4);
+
+            Assert.AreEqual(-2, v8.Numer);
+            Assert.AreEqual(3, v8.Denom);
+        }
+    }
+}
