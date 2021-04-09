611fbe4a1e8f530c13faf1f8e4b788c6c364ea57
tk-yoshimura <yoshimura8192@gmail.com>
2021-04-09 17:57:12 +0900

    sandbox double erf test

--- MultiPrecisionSandbox/Program.cs
+++ MultiPrecisionSandbox/Program.cs
@@ -1,34 +1,84 @@
 ﻿using MultiPrecision;
 using System;
+using System.IO;
 
 namespace MultiPrecisionSandbox {
     class Program {
         static void Main(string[] args) {
-            MultiPrecision<Pow2.N8> x1 = MultiPrecision<Pow2.N8>.Point5;
-            MultiPrecision<Pow2.N8> x2 = MultiPrecision<Pow2.N8>.BitDecrement(x1);
-            MultiPrecision<Pow2.N8> x3 = MultiPrecision<Pow2.N8>.BitDecrement(x2);
-            MultiPrecision<Pow2.N8> x4 = MultiPrecision<Pow2.N8>.BitDecrement(x3);
-            MultiPrecision<Pow2.N8> x5 = MultiPrecision<Pow2.N8>.BitDecrement(x4);
+            using (StreamWriter sw = new StreamWriter("../../erf_approx_nearzero.csv")) {
+                sw.WriteLine("z,y_true,y_approx,mp_error,double_error");
 
-            long n1 = (long)MultiPrecision<Pow2.N8>.Round(x1);
-            long n2 = (long)MultiPrecision<Pow2.N8>.Round(x2);
-            long n3 = (long)MultiPrecision<Pow2.N8>.Round(x3);
-            long n4 = (long)MultiPrecision<Pow2.N8>.Round(x4);
-            long n5 = (long)MultiPrecision<Pow2.N8>.Round(x5);
+                for (int z64 = 0; z64 <= 1 * 64; z64++) {
+                    double z = z64 / 64.0;
 
-            MultiPrecision<Pow2.N8> y1 = MultiPrecision<Pow2.N8>.CosHalfPI(x1);
-            MultiPrecision<Pow2.N8> y2 = MultiPrecision<Pow2.N8>.CosHalfPI(x2);
-            MultiPrecision<Pow2.N8> y3 = MultiPrecision<Pow2.N8>.CosHalfPI(x3);
-            MultiPrecision<Pow2.N8> y4 = MultiPrecision<Pow2.N8>.CosHalfPI(x4);
-            MultiPrecision<Pow2.N8> y5 = MultiPrecision<Pow2.N8>.CosHalfPI(x5);
+                    MultiPrecision<Pow2.N4> y_true = MultiPrecision<Pow2.N4>.Erf(z);
+                    double y_approx = ErrorFunction.ErfNearZero(z);
+                    MultiPrecision<Pow2.N4> mp_err = y_true - y_approx;
+                    double double_err = (double)y_true - y_approx;
 
-            Console.WriteLine(y1.ToHexcode());
-            Console.WriteLine(y2.ToHexcode());
-            Console.WriteLine(y3.ToHexcode());
-            Console.WriteLine(y4.ToHexcode());
-            Console.WriteLine(y5.ToHexcode());
+                    sw.WriteLine($"{z},{y_true},{y_approx},{mp_err},{double_err}");
+                }
+            }
 
-            Console.Read();
+            using (StreamWriter sw = new StreamWriter("../../erfc_approx_range12.csv")) {
+                sw.WriteLine("z,y_true,y_approx,mp_error,double_error");
+
+                for (int z64 = 1 * 64; z64 <= 2 * 64; z64++) {
+                    double z = z64 / 64.0;
+
+                    MultiPrecision<Pow2.N4> y_true = MultiPrecision<Pow2.N4>.Erfc(z);
+                    double y_approx = ErrorFunction.ErfcRange12(z);
+                    MultiPrecision<Pow2.N4> mp_err = y_true - y_approx;
+                    double double_err = (double)y_true - y_approx;
+
+                    sw.WriteLine($"{z},{y_true},{y_approx},{mp_err},{double_err}");
+                }
+            }
+
+            using (StreamWriter sw = new StreamWriter("../../erfc_approx_range23.csv")) {
+                sw.WriteLine("z,y_true,y_approx,mp_error,double_error");
+
+                for (int z64 = 2 * 64; z64 <= 3 * 64; z64++) {
+                    double z = z64 / 64.0;
+
+                    MultiPrecision<Pow2.N4> y_true = MultiPrecision<Pow2.N4>.Erfc(z);
+                    double y_approx = ErrorFunction.ErfcRange23(z);
+                    MultiPrecision<Pow2.N4> mp_err = y_true - y_approx;
+                    double double_err = (double)y_true - y_approx;
+
+                    sw.WriteLine($"{z},{y_true},{y_approx},{mp_err},{double_err}");
+                }
+            }
+
+            using (StreamWriter sw = new StreamWriter("../../erfc_approx_range34.csv")) {
+                sw.WriteLine("z,y_true,y_approx,mp_error,double_error");
+
+                for (int z64 = 3 * 64; z64 <= 4 * 64; z64++) {
+                    double z = z64 / 64.0;
+
+                    MultiPrecision<Pow2.N4> y_true = MultiPrecision<Pow2.N4>.Erfc(z);
+                    double y_approx = ErrorFunction.ErfcRange34(z);
+                    MultiPrecision<Pow2.N4> mp_err = y_true - y_approx;
+                    double double_err = (double)y_true - y_approx;
+
+                    sw.WriteLine($"{z},{y_true},{y_approx},{mp_err},{double_err}");
+                }
+            }
+
+            using (StreamWriter sw = new StreamWriter("../../erfc_approx_larger4.csv")) {
+                sw.WriteLine("z,y_true,y_approx,mp_error,double_error");
+
+                for (int z64 = 4 * 64; z64 <= 32 * 64; z64++) {
+                    double z = z64 / 64.0;
+
+                    MultiPrecision<Pow2.N4> y_true = MultiPrecision<Pow2.N4>.Erfc(z);
+                    double y_approx = ErrorFunction.ErfcLarger4(z);
+                    MultiPrecision<Pow2.N4> mp_err = y_true - y_approx;
+                    double double_err = (double)y_true - y_approx;
+
+                    sw.WriteLine($"{z},{y_true},{y_approx},{mp_err},{double_err}");
+                }
+            }
         }
     }
 }
