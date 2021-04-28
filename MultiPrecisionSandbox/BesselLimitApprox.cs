349746dee1a3e300776535a2e8870f3c7d6ef9e6
tk-yoshimura <yoshimura8192@gmail.com>
2021-04-29 01:16:17 +0900

    sandbox bessel

---
+++ MultiPrecisionSandbox/BesselLimitApprox.cs
@@ -0,0 +1,51 @@
+﻿using MultiPrecision;
+using System.Collections.Generic;
+
+namespace MultiPrecisionSandbox {
+    static class Bessel0LimitApprox<N> where N : struct, IConstant {
+        static readonly Dictionary<int, MultiPrecision<Plus1<N>>> table = new();
+
+        public static int BesselTermConvergence(int z) {
+            MultiPrecision<Plus1<N>> v = 1 / (MultiPrecision<Plus1<N>>)z;
+            MultiPrecision<Plus1<N>> w = v * v;
+
+            MultiPrecision<Plus1<N>> x = 0, u = 1, prev_c = null;
+
+            Sign sign = Sign.Plus;
+
+            for (int terms = 1; terms < int.MaxValue; terms++) {
+                MultiPrecision<Plus1<N>> c = u * Coef(terms * 2);
+
+                if (sign == Sign.Plus) {
+                    x += c;
+                }
+                else { 
+                    x -= c;
+                }
+
+                u *= w;
+
+                if (c.IsZero || x.Exponent - c.Exponent > MultiPrecision<N>.Bits) {
+                    return terms;
+                }
+                if (prev_c is not null && prev_c.Exponent < c.Exponent) {
+                    return int.MaxValue;
+                }
+
+                prev_c = c; 
+             }
+
+            return int.MaxValue;
+        }
+
+        private static MultiPrecision<Plus1<N>> Coef(int k) {
+            if (!table.ContainsKey(k)) {
+                MultiPrecision<Plus1<N>> c = Bessel0LimitCoef.Table(k).ToMultiPrecision<Plus1<N>>();
+
+                table.Add(k, c);
+            }
+
+            return table[k];
+        }
+    }
+}
---
+++ MultiPrecisionSandbox/BesselLimitCoef.cs
@@ -0,0 +1,61 @@
+﻿using MultiPrecision;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+
+namespace MultiPrecisionSandbox {
+    static class Bessel0LimitCoef {
+        private readonly static List<Fraction> a_table = new() { 1 };
+        private readonly static List<BigInteger> p_table = new() { 1 };
+        private readonly static List<BigInteger> f_table = new() { 1 };
+
+        public static Fraction Table(int n) {
+            if (n < 0) {
+                throw new ArgumentOutOfRangeException(nameof(n));
+            }
+
+            if (n < a_table.Count) {
+                return a_table[n];
+            }
+
+            for (int k = a_table.Count; k <= n; k++) {
+                BigInteger p = p_table.Last() * checked(-(2 * k - 1) * (2 * k - 1));
+                BigInteger f = f_table.Last() * checked(k * 8);
+
+                p_table.Add(p);
+                f_table.Add(f);
+                a_table.Add(new Fraction(p, f));
+            }
+
+            return a_table[n];
+        }
+    }
+
+    static class Bessel1LimitCoef {
+        private readonly static List<Fraction> a_table = new() { 1 };
+        private readonly static List<BigInteger> p_table = new() { 1 };
+        private readonly static List<BigInteger> f_table = new() { 1 };
+
+        public static Fraction Table(int n) {
+            if (n < 0) {
+                throw new ArgumentOutOfRangeException(nameof(n));
+            }
+
+            if (n < a_table.Count) {
+                return a_table[n];
+            }
+
+            for (int k = a_table.Count; k <= n; k++) {
+                BigInteger p = p_table.Last() * checked(4 - (2 * k - 1) * (2 * k - 1));
+                BigInteger f = f_table.Last() * checked(k * 8);
+
+                p_table.Add(p);
+                f_table.Add(f);
+                a_table.Add(new Fraction(p, f));
+            }
+
+            return a_table[n];
+        }
+    }
+}
