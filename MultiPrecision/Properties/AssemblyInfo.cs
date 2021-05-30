9c95732c28359c34860af57a1f2ef8812de608b1
tk-yoshimura <yoshimura8192@gmail.com>
2021-05-30 16:28:37 +0900

    Update AssemblyInfo.cs

--- MultiPrecision/Properties/AssemblyInfo.cs
+++ MultiPrecision/Properties/AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("4.5.*")]
+[assembly: AssemblyVersion("4.6.*")]
 
 [assembly: InternalsVisibleTo("MultiPrecisionTest")]
 [assembly: InternalsVisibleTo("MultiPrecisionBesselTest")]
