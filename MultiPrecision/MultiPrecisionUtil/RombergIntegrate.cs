0fb6b0df96b6cf3244ece0c08561b8d882452946
tk-yoshimura <yoshimura8192@gmail.com>
2021-06-11 00:23:25 +0900

    + integral

---
+++ MultiPrecision/MultiPrecisionUtil/RombergIntegrate.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace MultiPrecision {
+
+    public static partial class MultiPrecisionUtil {
+        public static (MultiPrecision<N> value, MultiPrecision<N> error) RombergIntegrate<N>(Func<MultiPrecision<N>, MultiPrecision<N>> f, 
+                                                                                             MultiPrecision<N> a, MultiPrecision<N> b, 
+                                                                                             int min_iterations = -1, int max_iterations = -1, 
+                                                                                             [AllowNull] MultiPrecision<N> epsilon = null)  where N : struct, IConstant {
+
+            if (f is null) {
+                throw new ArgumentNullException(nameof(f));
+            }
+            if (!(a <= b)) {
+                throw new ArgumentException($"{nameof(a)},{nameof(b)}");
+            }
+            if (min_iterations >= 0 && max_iterations >= 0 && min_iterations >= max_iterations) { 
+                throw new ArgumentException($"{nameof(min_iterations)},{nameof(max_iterations)}");
+            }
+
+            RichardsonExtrapolation<N> conv = new();
+            MultiPrecision<N> h = b - a;
+            MultiPrecision<N> s = 0, c = 0;
+
+            void kahan_sum(MultiPrecision<N> v) {
+                MultiPrecision<N> y = v - c;
+                MultiPrecision<N> t = s + y;
+                c = (t - s) - y;
+                s = t;
+            }
+
+            kahan_sum(f(a));
+            kahan_sum(f(b));
+
+            s /= 2;
+            c /= 2;
+
+            while (!conv.Epsilon.IsZero) {
+                for (MultiPrecision<N> x = a + h / 2; x < b; x += h) {
+                    kahan_sum(f(x));
+                }
+
+                h /= 2;
+
+                conv.Append(s * h);
+
+                if (min_iterations >= 0 && conv.Iterations < min_iterations) {
+                    continue;
+                }
+
+                if (max_iterations >= 0 && conv.Iterations >= max_iterations) {
+                    break;
+                }
+                if (epsilon is not null && conv.Epsilon < epsilon) {
+                    break;
+                }
+                if (!conv.Value.IsFinite || h.IsZero) {
+                    break;
+                }
+                if (conv.Epsilon.IsFinite && conv.Value.Exponent - conv.Epsilon.Exponent > MultiPrecision<N>.Bits - 8) {
+                    break;
+                }
+            }
+
+            return (conv.Value, conv.Epsilon);
+        }
+    }
+}
--- MultiPrecisionSandbox/Program.cs
+++ MultiPrecisionSandbox/Program.cs
@@ -4,31 +4,15 @@ using System;
 namespace MultiPrecisionSandbox {
     class Program {
         static void Main(string[] args) {
-            MultiPrecision<Pow2.N4> n = 0.75, k = 0;
+            //MultiPrecisionUtil.RichardsonExtrapolation<Pow2.N4>.R(5);
 
-            MultiPrecision<Pow2.N4> a = 1;
-            MultiPrecision<Pow2.N4> b = MultiPrecision<Pow2.N4>.Sqrt(1 - k * k);
-            MultiPrecision<Pow2.N4> p = MultiPrecision<Pow2.N4>.Sqrt(1 - n);
-            MultiPrecision<Pow2.N4> q = 1;
-            MultiPrecision<Pow2.N4> sum_q = 1;
+            //MultiPrecisionUtil.RichardsonExtrapolation<Pow2.N4> extrapolation = new();
+            //
+            //extrapolation.Append(1 / 2m);
+            //extrapolation.Append(17 / 64m);
+            //extrapolation.Append(197 / 1024m);
 
-            for (int i = 1; i <= 10; i++) {
-                MultiPrecision<Pow2.N4> a_next = (a + b) / 2;
-                MultiPrecision<Pow2.N4> b_next = MultiPrecision<Pow2.N4>.Sqrt(a * b);
-                MultiPrecision<Pow2.N4> p_next = (p * p + a * b) / (2 * p);
-                MultiPrecision<Pow2.N4> q_next = q / 2 * (p * p - a * b) / (p * p + a * b);
-
-                a = a_next;
-                b = b_next;
-                p = p_next;
-                q = q_next;
-
-                sum_q += q;
-
-                Console.WriteLine(q);
-            }
-
-            MultiPrecision<Pow2.N4> y = (2 + sum_q * n / (1 - n)) * MultiPrecision<Pow2.N4>.EllipticK(k) / 2;
+            (var value, var error) = MultiPrecisionUtil.RombergIntegrate<Pow2.N4>(MultiPrecision<Pow2.N4>.Log, 1, 2, min_iterations:8, epsilon:1e-4);
 
             Console.WriteLine("END");
             Console.Read();
