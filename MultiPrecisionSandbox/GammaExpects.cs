3c4d11c77a73d1bd54382e0c2e89cddeb10184fd
tk-yoshimura <yoshimura8192@gmail.com>
2021-12-15 16:19:24 +0900

    sandbox shifted legendre

---
+++ MultiPrecisionSandbox/GammaExpects.cs
@@ -0,0 +1,41 @@
+﻿using MultiPrecision;
+using System;
+using System.Collections.Generic;
+
+namespace MultiPrecisionSandbox {
+
+    static class GammaExpects<N> where N : struct, IConstant {
+        private static readonly MultiPrecision<N> sqrt_pi;
+        private static readonly Dictionary<int, MultiPrecision<Double<N>>> table;
+
+        static GammaExpects() {
+            sqrt_pi = MultiPrecision<N>.Sqrt(MultiPrecision<N>.PI);
+
+            table = new Dictionary<int, MultiPrecision<Double<N>>>();
+
+            table.Add(1, 1);
+            table.Add(2, 1);
+        }
+
+        public static MultiPrecision<N> Gamma(int z2) {
+            if (z2 < 1) {
+                throw new ArgumentException(nameof(z2));
+            }
+
+            static MultiPrecision<Double<N>> gamma(int i) {
+                if (table.ContainsKey(i)) {
+                    return table[i];
+                }
+
+                MultiPrecision<Double<N>> y = MultiPrecision<Double<N>>.Ldexp(i - 2, -1) * gamma(i - 2);
+
+                table.Add(i, y);
+                return y;
+            }
+
+            MultiPrecision<N> g = gamma(z2).Convert<N>();
+
+            return (z2 % 2 == 0) ? g : (g * sqrt_pi);
+        }
+    }
+}
--- MultiPrecisionSandbox/Program.cs
+++ MultiPrecisionSandbox/Program.cs
@@ -1,21 +1,59 @@
 ﻿using MultiPrecision;
 using System;
+using System.IO;
+using System.Numerics;
 
 namespace MultiPrecisionSandbox {
     class Program {
         static void Main(string[] args) {
-            //MultiPrecisionUtil.RichardsonExtrapolation<Pow2.N4>.R(5);
+            for (int n = 0; n <= 8; n++) {
+                for (int k = 0; k <= n; k++) {
+                    BigInteger v = ShiftedLegendre.Table(n, k);
 
-            //MultiPrecisionUtil.RichardsonExtrapolation<Pow2.N4> extrapolation = new();
-            //
-            //extrapolation.Append(1 / 2m);
-            //extrapolation.Append(17 / 64m);
-            //extrapolation.Append(197 / 1024m);
-
-            (var value, var error) = MultiPrecisionUtil.RombergIntegrate<Pow2.N4>(MultiPrecision<Pow2.N4>.Log, 1, 2);
+                    Console.Write($"{v} ");
+                }
+                Console.Write("\n");
+            }
 
             Console.WriteLine("END");
             Console.Read();
+        }
+
+        private static void GammaApproxSummary<N>(string filepath) where N : struct, IConstant {
+            Console.WriteLine(MultiPrecision<N>.Length);
+
+            using (StreamWriter sw = new StreamWriter(filepath)) {
+                for (int terms = 4; terms < 32; terms++) {
+                    sw.WriteLine($"terms : {terms}");
+
+                    for (int z2 = 1; z2 <= 20; z2++) {
+                        MultiPrecision<N> z = MultiPrecision<N>.Ldexp(z2, -1);
+
+                        MultiPrecision<N> y_expected = GammaExpects<N>.Gamma(z2);
+                        MultiPrecision<N> y_actual = SterlingApprox<N>.Gamma(z, terms);
+
+                        MultiPrecision<N> err = MultiPrecision<N>.Abs(y_expected - y_actual);
+
+                        sw.WriteLine($"  gamma({z2 * 0.5:F1})");
+                        sw.WriteLine($"    true   : {y_expected}");
+                        sw.WriteLine($"    approx : {y_actual}");
+                        sw.WriteLine($"    err    : {err}");
+
+                        if (y_expected != y_actual) {
+                            for (int roundbits = 0; roundbits < MultiPrecision<N>.Bits; roundbits++) {
+                                if (MultiPrecision<N>.RoundMantissa(y_expected, roundbits) == MultiPrecision<N>.RoundMantissa(y_actual, roundbits)) {
+                                    sw.WriteLine($"    matchbits : {MultiPrecision<N>.Bits - roundbits}");
+                                    break;
+                                }
+                            }
+                        }
+
+                        Console.Write(".");
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Empty);
         }
     }
 }
